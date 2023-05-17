30438b8f555dc9e5a82167b3af482e5f2c2129ff
Marius-Elting <Marius.Supercode@gmail.com>
2023-05-17 22:09:08 +0200

    hello world

---
+++ HelloWorld/src/SecondClass.java
@@ -0,0 +1,7 @@
+public class SecondClass {
+    public static void main(String[] args) {
+        // println will add a blank line after printing the text
+        System.out.println("Hello World 2");
+        System.out.print("Hello World 3");
+    }
+}
