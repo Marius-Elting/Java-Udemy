6d67a2dd7044810244f710cf52fe810d4540b8bd
Marius-Elting <Marius.Supercode@gmail.com>
2023-05-17 22:13:18 +0200

    If Then

---
+++ IfThen/src/IfThen.java
@@ -0,0 +1,13 @@
+public class IfThen {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+        boolean isAlien = true; 
+        // alternative if(isAlien == true)
+        if(isAlien)
+            System.out.print("Hello I'm an Alien");
+    }
+};
+
+
+
