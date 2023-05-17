17fd42cc2534bb6dfa6ec98ee4e4ccf5424d2f25
Marius-Elting <Marius.Supercode@gmail.com>
2023-05-17 22:16:49 +0200

    logical AND & logical OR

--- IfThen/src/IfThen.java
+++ IfThen/src/IfThen.java
@@ -1,12 +1,18 @@
 public class IfThen {
     public static void main(String[] args) {
-        System.out.println("Hello World");
 
         boolean isAlien = true; 
+        boolean isAnimal = false;
         // alternative if(isAlien == true)
         if(isAlien)
-            System.out.print("Hello I'm an Alien");
-    }
+            System.out.println("Hello I'm an Alien");
+
+        if(isAlien || isAnimal)
+            System.out.println("Its an Animal or an Alien");
+            
+        if(isAlien && !isAnimal)
+            System.out.println("Its not an Animal but an Alien");
+        }
 };
 
 
