8ac32ea9014941e4b5aa6109906b55f3bc86b0fc
Marius-Elting <Marius.Supercode@gmail.com>
2023-05-21 20:54:59 +0200

    finished exercises 30 & 31

---
+++ Section7 (OOP Part 1 - Inheritance)/Exercises/30/src/SumCalculatorMain.java
@@ -0,0 +1,9 @@
+public class SumCalculatorMain {
+    public static void main(String[] args) {
+        SimpleCalculator calc = new SimpleCalculator();
+        calc.setFirstNumber(1);
+        calc.setSecondNumber(5);
+
+        calc.getMultiplicationResult();
+    }
+}
---
+++ Section7 (OOP Part 1 - Inheritance)/Exercises/31/src/PersonMain.java
@@ -0,0 +1,10 @@
+public class PersonMain {
+    public static void main(String[] args) {
+        Person me = new Person();
+        me.setAge(21);
+        me.setFirstName("Marius");
+        me.setLastName("Elting");
+
+        me.getFullName();
+    }
+}
