5f4416b5384046e30048db0ba9ab76d1cfa7a334
Marius-Elting <Marius.Supercode@gmail.com>
2023-05-22 12:58:23 +0200

    finished Inheritance part 1

---
+++ Section7 (OOP Part 1 - Inheritance)/Inheritance/src/InerhitanceMain.java
@@ -0,0 +1,16 @@
+public class InerhitanceMain {
+    public static void main(String[] args) {
+        Animal animal = new Animal("Generic Animal", "Huge", 400);
+        doAnimalStuff(animal, "super Slow");
+
+        Dog dog = new Dog();
+        doAnimalStuff(dog, "Fast");
+    }
+
+    public static void doAnimalStuff(Animal animal, String speed) {
+        animal.makeNoise();
+        animal.move(speed);
+        System.out.println(animal);
+        System.out.println("_ _ _ _");
+    }
+}
