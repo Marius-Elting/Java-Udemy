348f6065025c08896c1ad1bdbbc2be10538c8d00
Marius-Elting <Marius.Supercode@gmail.com>
2023-05-22 18:40:51 +0200

    finished Inheritance Lecture 2-3

--- Section7 (OOP Part 1 - Inheritance)/Inheritance/src/InerhitanceMain.java
+++ Section7 (OOP Part 1 - Inheritance)/Inheritance/src/InerhitanceMain.java
@@ -5,11 +5,20 @@ public class InerhitanceMain {
 
         Dog dog = new Dog();
         doAnimalStuff(dog, "Fast");
+
+        Dog yorkie = new Dog("Yorkie", 15);
+        doAnimalStuff(yorkie, "fast");
+        Dog retriever = new Dog("Labrador Retriever", 65, "Floppy", "slow");
+        doAnimalStuff(retriever, "slow");
+
+        Dog wolf = new Dog("Wolf", 40);
+        doAnimalStuff(wolf, "fast");
     }
 
     public static void doAnimalStuff(Animal animal, String speed) {
         animal.makeNoise();
         animal.move(speed);
+
         System.out.println(animal);
         System.out.println("_ _ _ _");
     }
