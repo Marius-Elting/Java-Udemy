c3558ff7dd03c93e9f5c4a62b40ab21fed93713a
Marius-Elting <Marius.Supercode@gmail.com>
2023-05-24 14:08:37 +0200

    finished Polymorphism Lecture

---
+++ Section8 (OOP Part 2 - Polymorphism)/Polymorphism/src/Movie.java
@@ -0,0 +1,61 @@
+public class Movie {
+    private String title;
+
+    public Movie(String title) {
+        this.title = title;
+    }
+
+    public void watchMovie() {
+        String instanceType = this.getClass().getSimpleName();
+        System.out.println(title + " is a " + instanceType + " film");
+    }
+
+    public static Movie getMovie(String type, String title) {
+        return switch (type.toUpperCase().charAt(0)) {
+            case 'S' -> new ScienceFiction(title);
+            case 'A' -> new Adventure(title);
+            case 'C' -> new Comedy(title);
+            default -> new Movie(title);
+        };
+    }
+
+}
+
+class Adventure extends Movie {
+    public Adventure(String title) {
+        super(title);
+    }
+
+    @Override
+    public void watchMovie() {
+        super.watchMovie();
+        System.out.printf(".. %s%n".repeat(3), "Pleasant Scene", "Scary Music",
+                "Something Bad Happens");
+    }
+}
+
+class Comedy extends Movie {
+    public Comedy(String title) {
+        super(title);
+    }
+
+    @Override
+    public void watchMovie() {
+        super.watchMovie();
+        System.out.printf(".. %s%n".repeat(3), "Funny Scene", "Funny Music",
+                "Something Funny Happens");
+    }
+}
+
+class ScienceFiction extends Movie {
+    public ScienceFiction(String title) {
+        super(title);
+    }
+
+    @Override
+    public void watchMovie() {
+        super.watchMovie();
+        System.out.printf(".. %s%n".repeat(3), "Craty Scene", "Alien Music",
+                "Alien stuff Happens");
+    }
+}
