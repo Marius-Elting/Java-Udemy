3f9cb65f03ade1bd0e325ba5dfd649c239f41777
Marius-Elting <Marius.Supercode@gmail.com>
2023-05-24 11:12:14 +0200

    finished Encapsulation lecture 1&2

---
+++ Section8 (OOP Part 2 - Polymorphism)/Encapsulation/src/EncapsulationMain.java
@@ -0,0 +1,22 @@
+import javax.swing.text.Style;
+
+public class EncapsulationMain {
+    public static void main(String[] args) {
+        // Player player = new Player();
+        // player.fullName = "Tim";
+        // player.health = 20;
+        // player.weapon = "Sword";
+
+        // int damage = 10;
+
+        // player.loseHealth(damage);
+        // System.out.println(player.healthRemaining());
+        // player.health = 100;
+        // player.loseHealth(11);
+        // System.out.println(player.healthRemaining());
+
+        EnhancedPlayer tim = new EnhancedPlayer("TIM", 200, "Sword");
+
+        System.out.println(tim.healthRemaining());
+    }
+}
