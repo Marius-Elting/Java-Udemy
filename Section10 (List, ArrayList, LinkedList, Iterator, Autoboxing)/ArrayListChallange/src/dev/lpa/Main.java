149f60574a9995fec17a13e486c2b76dcab207af
Marius-Elting <Marius.Supercode@gmail.com>
2023-05-26 22:37:05 +0200

    finished ArrayList Challange

---
+++ Section10 (List, ArrayList, LinkedList, Iterator, Autoboxing)/ArrayListChallange/src/dev/lpa/Main.java
@@ -0,0 +1,57 @@
+package dev.lpa;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ArrayList<String> shoppingList = new ArrayList<>();
+        Scanner s = new Scanner(System.in);
+        boolean active = true;
+        System.out.println("Welcome to your Shopping list");
+        System.out.println("0  exit");
+        System.out.println("1 - add Items");
+        System.out.println("2 - remove Items");
+        while (active) {
+
+            int action = Integer.parseInt(s.nextLine());
+
+            if (action == 0) {
+                active = false;
+            }
+            if (action == 1) {
+                System.out.println("Type items to add seperated by komma");
+                String input = s.nextLine();
+                String[] inputArray = input.split(",");
+                for (String item : inputArray) {
+                    if (shoppingList.contains(item)) {
+                        shoppingList.remove(item);
+                    } else {
+                        shoppingList.add(item);
+                        System.out.print(item + " ");
+                    }
+                }
+                System.out.println("has been added!");
+
+            }
+            if (action == 2) {
+                System.out.println("Type items to remove seperated by komma");
+                String input = s.nextLine();
+                String[] inputArray = input.split(",");
+                for (String item : inputArray) {
+                    if (shoppingList.contains(item)) {
+                        shoppingList.remove(item);
+                        System.out.print(item + ", ");
+                    }
+                }
+                System.out.println("has been removed!");
+            }
+        }
+
+        System.out.println(shoppingList);
+        System.out.println(shoppingList.get(0));
+        s.close();
+    }
+}
